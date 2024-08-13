9f070fd08d795d4c6df12889f71e383f1ddde31a
Israel-SoaresPorto <isaelporto15@gmail.com>
2024-08-13 12:04:21 -0300

    adiconando alternancia entre cronometro/timer

--- js/main.js
+++ js/main.js
@@ -2,8 +2,20 @@ import cronometer from "./cronometer.js";
 import timer from "./timer.js";
 
 const mainContainer = document.querySelector('.container');
+const cronometerButton = document.querySelector('#cronometer');
+const timerButton = document.querySelector('#timer');
 
 const cronometerPage = cronometer();
 const timerPage = timer();
 
-mainContainer.appendChild(timerPage);
+cronometerButton.addEventListener('click', () => {
+    mainContainer.innerHTML = '';
+    mainContainer.appendChild(cronometerPage);
+});
+
+timerButton.addEventListener('click', () => {
+    mainContainer.innerHTML = '';
+    mainContainer.appendChild(timerPage);
+});
+
+mainContainer.appendChild(cronometerPage);
