56a7919c9b8b27b60a1940304679d0593418ee9c
Israel-SoaresPorto <isaelporto15@gmail.com>
2024-08-14 08:22:16 -0300

    salvando alterações

--- js/Timer.js
+++ js/Timer.js
@@ -1,6 +1,6 @@
-import button from "./Button.js";
+import Button from "./Button.js";
 
-export default function timer() {
+/* export default function timer() {
   const timerPage = document.createElement("div");
   timerPage.classList.add("timer");
 
@@ -113,4 +113,103 @@ export default function timer() {
     
 
   return timerPage;
+} */
+
+export default class Timer {
+  constructor() {
+    this.timerInterval;
+    this.duration = 0;
+  }
+
+  render() {
+    const timerPage = document.createElement("div");
+    timerPage.classList.add("timer");
+
+    timerPage.innerHTML = `
+            <form class="timer__form">
+                <div class="timer__form__inputs">
+                    <input type="number" id="hours-input" min="0" max="99">
+                    <label for="hours-input">h</label>
+                    <input type="number" id="minutes-input" min="0" max="59" >
+                    <label for="minutes-input">m</label>
+                    <input type="number" id="seconds-input" min="0" max="59">
+                    <label for="seconds-input">s</label>
+                </div>
+                <button type="submit">Iniciar</button>
+            </form>    
+            <div class="display">
+                <span id="hours">00</span>
+                <span class="colon">:</span>
+                <span id="minutes">00</span>
+                <span class="colon">:</span>
+                <span id="seconds">00</span>
+            </div>
+            <div class="timer__controls">
+                ${new Button("play", "play", true).render()}
+                ${new Button("pause", "pause", true).render()}
+                ${new Button("stop", "stop", true).render()}
+            </div>
+        `;
+
+    const form = timerPage.querySelector(".timer__form");
+    const display = timerPage.querySelector(".display");
+    const playButton = timerPage.querySelector("#play");
+    const pauseButton = timerPage.querySelector("#pause");
+    const stopButton = timerPage.querySelector("#stop");
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const hours = parseInt(form.querySelector("#hours-input").value) || 0;
+      const minutes = parseInt(form.querySelector("#minutes-input").value) || 0;
+      const seconds = parseInt(form.querySelector("#seconds-input").value) || 0;
+
+      this.duration = (hours * 60 * 60) + (minutes * 60) + seconds;
+
+      if (this.duration <= 0) {
+        return;
+      }
+
+      form.reset();
+      this.timerInterval = setInterval(() => this.displayTime(display), 1000);
+      this.displayTime(display);
+
+      form.querySelector("button").disabled = true;
+      this.enableButtons(playButton);
+    });
+
+    return timerPage;
+  }
+
+  disableButtons(...buttons) {
+    buttons.forEach((button) => (button.disabled = true));
+  }
+
+  enableButtons(...buttons) {
+    buttons.forEach((button) => (button.disabled = false));
+  }
+
+  displayTime(display) {
+    let remaningTime = this.duration;
+
+    const hours = Math.floor(remaningTime / 3600);
+    const minutes = Math.floor((remaningTime / 60) % 60);
+    const seconds = Math.floor(remaningTime % 60);
+    display.querySelector("#hours").textContext =
+      hours < 10 ? `0${hours}` : hours;
+    display.querySelector("#minutes").textContext =
+      minutes < 10 ? `0${minutes}` : minutes;
+    display.querySelector("#seconds").textContext =
+      seconds < 10 ? `0${seconds}` : seconds;
+
+    console.log(remaningTime);  
+
+    if (remaningTime <= 0) {
+      clearInterval(this.timerInterval);
+      form.querySelector("button").disabled = false;
+      this.disableButtons(playButton, pauseButton, stopButton);
+    }
+
+    this.duration--;
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 import Cronometer from "./Cronometer.js";
-import timer from "./timer.js";
+import Timer from "./Timer.js";
 
 const mainContainer = document.querySelector('.container');
+const cronometerButton = document.querySelector('#cronometer');
+const timerButton = document.querySelector('#timer');
 
 const cronometerPage = new Cronometer();
+const timerPage = new Timer();
 
-/* cronometerButton.addEventListener('click', () => {
+cronometerButton.addEventListener('click', () => {
     mainContainer.innerHTML = '';
-    mainContainer.appendChild(cronometerPage);
+    mainContainer.appendChild(cronometerPage.render());
 });
 
 timerButton.addEventListener('click', () => {
     mainContainer.innerHTML = '';
-    mainContainer.appendChild(timerPage);
-}); */
+    mainContainer.appendChild(timerPage.render());
+});
 
-mainContainer.appendChild(cronometerPage.render());
+mainContainer.appendChild(timerPage.render());
