f2d49ff3848d58a338f8c0838497a52d079f43df
Israel-SoaresPorto <isaelporto15@gmail.com>
2024-08-18 15:17:03 -0300

    refazendo lógica do timer

---
+++ js/timer.js
@@ -0,0 +1,100 @@
+const modal = document.querySelector(".timer__modal");
+const timerForm = document.querySelector(".modal__form");
+const setTimerButton = document.querySelector(".timer__open-modal");
+const display = document.querySelector(".display");
+const playButton = document.querySelector("#play");
+const pauseButton = document.querySelector("#pause");
+const stopButton = document.querySelector("#stop");
+
+let timerInterval;
+let duration = 0;
+
+function disableButtons(...buttons) {
+  buttons.forEach((button) => (button.disabled = true));
+}
+
+function enableButtons(...buttons) {
+  buttons.forEach((button) => (button.disabled = false));
+}
+
+function displayTime() {
+  let remaningTime = duration;
+
+  const hours = Math.floor(remaningTime / 3600);
+  const minutes = Math.floor((remaningTime / 60) % 60);
+  const seconds = Math.floor(remaningTime % 60);
+
+  display.querySelector("#hours").textContent =
+    hours < 10 ? `0${hours}` : hours;
+  display.querySelector("#minutes").textContent =
+    minutes < 10 ? `0${minutes}` : minutes;
+  display.querySelector("#seconds").textContent =
+    seconds < 10 ? `0${seconds}` : seconds;
+
+  document.title = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds} | Timer`;
+
+  if (remaningTime <= 0) {
+    clearInterval(timerInterval);
+    setTimerButton.disabled = false;
+    this.disableButtons(...document.querySelectorAll("button__control"));
+    document.title = "Timer";
+  }
+
+  duration--;
+}
+
+modal.querySelector(".modal__header__close").addEventListener("click", () => {
+  modal.close();
+});
+
+timerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const hours = parseInt(timerForm.hours.value) || 0;
+  const minutes = parseInt(timerForm.minutes.value) || 0;
+  const seconds = parseInt(timerForm.seconds.value) || 0;
+
+  duration = hours * 3600 + minutes * 60 + seconds;
+
+  if (this.duration <= 0) {
+    return;
+  }
+
+  timerInterval = setInterval(() => displayTime(), 1000);
+
+  timerForm.reset();
+  modal.close();
+  setTimerButton.disabled = true;
+  enableButtons(pauseButton, stopButton);
+});
+
+timerForm.addEventListener("reset", () => {
+  modal.close();
+});
+
+setTimerButton.addEventListener("click", () => {
+  modal.showModal();
+});
+
+playButton.addEventListener("click", () => {
+  timerInterval = setInterval(() => displayTime(display), 1000);
+  disableButtons(playButton);
+  enableButtons(pauseButton);
+});
+
+pauseButton.addEventListener("click", () => {
+  clearInterval(timerInterval);
+  disableButtons(pauseButton);
+  enableButtons(playButton);
+});
+
+stopButton.addEventListener("click", () => {
+  clearInterval(timerInterval);
+  duration = 0;
+  displayTime(display);
+  setTimerButton.disabled = false;
+  disableButtons(playButton, pauseButton, stopButton);
+  document.title = "Timer";
+});
